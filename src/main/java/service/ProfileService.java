12be2f270f80320cebe74bbcb50079283bc1cdc7
yujin0131 <lyujin0131@naver.com>
2020-09-17 10:37:11 +0900

    Create profilepage and Fix uploadpage

---
+++ src/main/java/service/ProfileService.java
@@ -0,0 +1,28 @@
+package service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dao.ProfileDAO;
+import vo.ProfileVO;
+
+@Service("profileService")
+public class ProfileService {
+	@Autowired
+	ProfileDAO profile_dao;
+
+	public int upload(ProfileVO vo) {
+		int res = profile_dao.insert(vo);
+		return res;
+	}
+
+	public List<ProfileVO> select(int user_idx) {
+		List<ProfileVO> list = profile_dao.select(user_idx);
+		return list;
+	}
+
+
+
+}

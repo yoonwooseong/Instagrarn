9af62ab399b6491e2440ec96d845659fc55b9494
yoonwooseong <57824259+yoonwooseong@users.noreply.github.com>
2020-09-25 18:29:39 +0900

    Apply like btn alert
    Apply like btn alert

--- src/main/java/com/bis/instagrarn/HomeController.java
+++ src/main/java/com/bis/instagrarn/HomeController.java
@@ -103,12 +103,12 @@ public class HomeController {
 	
 	@RequestMapping(value = "/add_reply", method = RequestMethod.GET)
 	@ResponseBody
-	public int add_post(Model model, int board_idx, String reply) {
+	public int add_reply(Model model, int board_idx, String reply) {
 		int user_idx = 2;
-		int from_user_idx = 2;
+		int from_user_idx = user_idx;
 		int to_user_idx = 1;
 		String alert_type = "reply";
-		int res = profileService.add_reply(board_idx, user_idx, reply);
+		profileService.add_reply(board_idx, user_idx, reply);
 		profileService.add_alert(from_user_idx, to_user_idx, alert_type);
 		return board_idx;
 	}
@@ -117,7 +117,11 @@ public class HomeController {
 	@ResponseBody
 	public int clickLike(Model model, int board_idx) {
 		int user_idx = 2;
-		int res = profileService.clicked_like(board_idx, user_idx);
+		int from_user_idx = user_idx;
+		int to_user_idx = 1;
+		String alert_type = "like";
+		profileService.clicked_like(board_idx, user_idx);
+		profileService.add_alert(from_user_idx, to_user_idx, alert_type);
 		return board_idx;
 	}
 	
